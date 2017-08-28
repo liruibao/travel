71918e8d5db4bea86815de575da656328a9ab7bc
liruibao <silentlrb@gmail.com>
2017-08-28 23:08:34 +0800

    添加全局处理page

---
+++ app/src/main/java/com/ssoft/travel/pager/ExceptionPage.java
@@ -0,0 +1,19 @@
+package com.ssoft.travel.pager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ssoft.travel.R;
+
+/**
+ * Created by Administrator on 2017/8/28 0028.
+ */
+
+public class ExceptionPage extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_exception);
+    }
+}
